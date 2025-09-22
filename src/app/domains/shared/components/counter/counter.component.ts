7636084446f6fdec929f06dea0d65f091f834fc1
Raisa Toscano <daisa1690@gmail.com>
2025-09-21 19:41:45 -0600

    Update counter and about components to use new input signal instead of @Input decorator, remove ngOnchages method replacing it for effects and computed signals.

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,12 +1,12 @@
 import {
   Component,
-  Input,
-  SimpleChanges,
   signal,
-  OnChanges,
   OnInit,
   AfterViewInit,
   OnDestroy,
+  input,
+  effect,
+  computed,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -15,11 +15,10 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './counter.component.html',
 })
-export class CounterComponent
-  implements OnChanges, OnInit, AfterViewInit, OnDestroy
-{
-  @Input({ required: true }) duration = 0;
-  @Input({ required: true }) message = '';
+export class CounterComponent implements OnInit, AfterViewInit, OnDestroy {
+  duration = input.required<number>();
+  doubleDuration = computed(() => this.duration() * 2);
+  message = input.required<string>();
   counter = signal(0);
   counterRef: number | undefined;
 
@@ -29,17 +28,11 @@ export class CounterComponent
     // una vez
     console.log('constructor');
     console.log('-'.repeat(10));
-  }
 
-  ngOnChanges(changes: SimpleChanges) {
-    // before and during render
-    console.log('ngOnChanges');
-    console.log('-'.repeat(10));
-    console.log(changes);
-    const duration = changes['duration'];
-    if (duration && duration.currentValue !== duration.previousValue) {
-      this.doSomething();
-    }
+    effect(() => {
+      this.message();
+      this.doSomethingTwo();
+    });
   }
 
   ngOnInit() {
@@ -48,8 +41,8 @@ export class CounterComponent
     // async, then, subs
     console.log('ngOnInit');
     console.log('-'.repeat(10));
-    console.log('duration =>', this.duration);
-    console.log('message =>', this.message);
+    console.log('duration =>', this.duration());
+    console.log('message =>', this.message());
     this.counterRef = window.setInterval(() => {
       console.log('run interval');
       this.counter.update((statePrev) => statePrev + 1);
@@ -69,8 +62,8 @@ export class CounterComponent
     window.clearInterval(this.counterRef);
   }
 
-  doSomething() {
-    console.log('change duration');
+  doSomethingTwo() {
+    console.log('change duration two');
     // async
   }
 }
